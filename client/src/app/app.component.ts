e7e3ae9e9fe3b94870bf4cf675271c9fffad0a7d
Kenneth.Nnalue <kennethnnalue@gmaiul.com>
2022-11-14 15:00:08 +0100

    Add Angular

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: any | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (result) => {
+        this.users = result;
+        console.log(this.users);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+}
